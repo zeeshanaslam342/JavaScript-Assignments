8c2e62686950ff1ab0ee70411cfaa81406231997
Zeeshan Aslam <121743591+Zeeshan-Rajputt@users.noreply.github.com>
2023-01-25 23:17:28 +0500

    Add Answer's of chapter 1 (alerts) Question's

--- Javascript_Assignments/app.js
+++ Javascript_Assignments/app.js
@@ -1,2 +1,24 @@
 // =======================Chapter # 01 (Alerts)====================
 //                           Question # 01
+
+// alert ("Welcome to my Website");
+
+//                           Question # 02
+
+// alert ("Error! Plaese enter a valid password.");
+
+//                           Question # 03
+
+// alert ("Welcome to Js Land...\n Happy Coding!");
+
+//                           Question # 04
+
+// alert ("Welcome to Js Land...");
+// alert ("Happy Coding!");
+
+//                           Question # 05
+
+// alert ("Hello...I can run JS through my web browser's console");
+
+//                           Question # 06
+
