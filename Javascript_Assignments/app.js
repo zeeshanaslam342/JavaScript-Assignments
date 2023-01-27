20e81595180bdb5a8a1bc880ac35c7a218d982ae
Zeeshan Aslam <121743591+Zeeshan-Rajputt@users.noreply.github.com>
2023-01-27 23:52:11 +0500

    Answer's of Assignment_No-2 Chapter_No-2

--- Javascript_Assignments/app.js
+++ Javascript_Assignments/app.js
@@ -1,4 +1,7 @@
+// *************************Assignment_No_01**************************
+
 // =======================Chapter # 01 (Alerts)====================
+
 //                           Question # 01
 
 // alert ("Welcome to my Website");
@@ -20,5 +23,65 @@
 
 // alert ("Hello...I can run JS through my web browser's console");
 
+
+// *************************Assignment_No_02**************************
+
+// =======================Chapter # 02 (Variables for Strings)====================
+
+//                           Question # 01
+
+// var userName;
+
+
+//                           Question # 02
+
+// var myname = "Zeeshan Rajput";
+
+
+//                           Question # 03
+
+// var message;
+// message = "Hello World";
+// alert (message);
+
+
+//                           Question # 04
+
+// var fullname = "Zeeshan Rajput";
+// var age = "30 years old";
+// var courseName = "Certified Mobile Application Development";
+
+// alert (fullname);
+// alert (age);
+// alert (courseName);
+
+
+//                           Question # 05
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert (pizza);
+
+
 //                           Question # 06
 
+// var myEmail = "My email address is"
+// var email = " m.zeeshan342@gmail.com";
+// alert (myEmail + email);
+
+
+//                           Question # 07
+
+// var book = "A smarter way to learn JavaScript";
+// alert ("I am trying to learn fron the Book " + book);
+
+
+//                           Question # 08
+
+// document.write ("Yah! I can write HTML content through JavaScript");
+
+
+//                           Question # 09
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert (design);
+// document.write (design);
