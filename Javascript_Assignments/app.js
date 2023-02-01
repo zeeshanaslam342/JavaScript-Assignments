3de3cf52f7984881dbe54be6c23092a0cc7498b9
Zeeshan Aslam <121743591+Zeeshan-Rajputt@users.noreply.github.com>
2023-02-02 00:15:03 +0500

    Answer's of Assignment_No-3 Chapter_No-5_1-to-7

--- Javascript_Assignments/app.js
+++ Javascript_Assignments/app.js
@@ -1,4 +1,4 @@
-// *************************Assignment_No_01**************************
+// *************************Assignment_No_01_Chapter 1**************************
 
 // =======================Chapter # 01 (Alerts)====================
 
@@ -24,7 +24,7 @@
 // alert ("Hello...I can run JS through my web browser's console");
 
 
-// *************************Assignment_No_02**************************
+// *************************Assignment_No_02_Chapter 2 to 4**************************
 
 // =======================Chapter # 02 (Variables for Strings)====================
 
@@ -117,7 +117,7 @@
 
 
 
-// =======================Chapter # 0 (Variables Names:Legal & Illegal)====================
+// =======================Chapter # 04 (Variables Names:Legal & Illegal)====================
 
 
 //                           Question # 01
@@ -151,3 +151,119 @@
 // document.write ("<p>Vriable names are case Sensitive.</p>")
 
 // document.write ("<p>Variables names should not be JS Keyword.</p>")
+
+
+
+// *************************Assignment_No_03_Chapter 5 to 8**************************
+
+// =======================Chapter # 05 (Math expressions:Familiar operators)====================
+
+//                           Question # 01
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// document.write ("<h1>" + "Sum of " + num1 + " and " + num2 + " is " + sum + "</h1>");
+
+
+//                           Question # 02(a)
+
+// var num1 = 5;
+// var num2 = 3;
+// var subtract = num1 - num2;
+// document.write ("<h1>" + "Subtract from " + num1 + " to " + num2 + " is " + subtract + "</h1>");
+
+
+//                           Question # 02(b)
+
+// var num1 = 3;
+// var num2 = 5;
+// var multiplication = num1 * num2;
+// document.write ("<h1>" + "Multiplication of " + num1 + " or " + num2 + " is " + multiplication + "</h1>");
+
+
+//                           Question # 02(c)
+
+// var num1 = 6;
+// var num2 = 3;
+// var division = num1 / num2;
+// document.write ("<h1>" + "Division of " + num1 + " and " + num2 + " is " + division + "</h1>");
+
+
+//                           Question # 02(d)
+
+// var num1 = 10;
+// var num2 = 3;
+// var modulus = num1 % num2;
+// document.write ("<h1>" + "Modulus of " + num1 + " and " + num2 + " is " + modulus + "</h1>");
+
+
+//                           Question # 03
+
+// var num;
+// document.write ("<h3>" + "Value after variable declaration is: " + num + "</h3>");
+
+// num = 5;
+// document.write ("<h3>" + "Initial value: " + num + "</h3>");
+
+// num = ++num;
+// document.write ("<h3>" + "Value after increment is: " + num + "</h3>");
+
+// num = num + 7;
+// document.write ("<h3>" + "Value after addition is: " + num + "</h3>");
+
+// num = --num;
+// document.write ("<h3>" + "Value after decrement is: " + num + "</h3>");
+
+// num = num % 3;
+// document.write ("<h3>" + "The remainder is : " + num + "</h3>");
+
+
+
+//                           Question # 04
+
+// var ticketPrice = 600;
+// var totalCost = ticketPrice * 5;
+// document.write ("<h3>" + "Total cost to buy 5 tickets to a movie is " + totalCost + " PKR");
+
+
+//                           Question # 05
+
+// document.write ("<h1> Tabel of 5 </h1>");
+// document.write ("5 x 1 = " + 5*1 + "<br>");
+// document.write ("5 x 2 = " + 5*2 + "<br>");
+// document.write ("5 x 3 = " + 5*3 + "<br>");
+// document.write ("5 x 4 = " + 5*4 + "<br>");
+// document.write ("5 x 5 = " + 5*5 + "<br>");
+// document.write ("5 x 6 = " + 5*6 + "<br>");
+// document.write ("5 x 7 = " + 5*7 + "<br>");
+// document.write ("5 x 8 = " + 5*8 + "<br>");
+// document.write ("5 x 9 = " + 5*9 + "<br>");
+// document.write ("5 x 10 = " + 5*10);
+
+
+//                           Question # 06
+
+
+
+//                           Question # 07
+
+// var item_1_Price = 650;
+// var item_2_Price = 100;
+// var quantityItem_1 = 3;
+// var quantityItem_2 = 7;
+// var shppingChr = 100;
+
+// var totalCostItem_1 = item_1_Price * quantityItem_1;
+// var totalCostItem_2 = item_2_Price * quantityItem_2;
+// var totalCostOfOrder = totalCostItem_1 + totalCostItem_2 + shppingChr;
+
+// document.write ("<h1>Shopping Cart</h1>");
+// document.write ("Price of item 1 is " + item_1_Price + "<br>");
+// document.write ("Quantity of item 1 is " + quantityItem_1 + "<br>");
+// document.write ("Price of item 2 is " + item_2_Price + "<br>");
+// document.write ("Quantity of item 2 is " + quantityItem_2 + "<br>");
+// document.write ("Shipping Charges " + shppingChr + "<br><br><br>");
+// document.write ("Total cost of your order is " + totalCostOfOrder);
+
+
