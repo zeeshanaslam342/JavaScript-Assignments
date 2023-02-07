d629da4b6453a3cd5ffdf7ee980dff5a9757925e
Zeeshan Aslam <121743591+Zeeshan-Rajputt@users.noreply.github.com>
2023-02-07 17:40:07 +0500

    Answer's of Assignment_No-4 Chapter 9 to 11

--- Javascript_Assignments/app.js
+++ Javascript_Assignments/app.js
@@ -154,7 +154,7 @@
 
 
 
-// *************************Assignment_No_03_Chapter 5 to 8**************************
+// *************************Assignment_No_03_Chapter 5 and 6 to 9**************************
 
 // =======================Chapter # 05 (Math expressions:Familiar operators)====================
 
@@ -277,7 +277,7 @@
 // document.write ("Total marks: " + totalMarks + "<br>");
 // document.write ("Marks obtained: " + obtMarks + "<br>");
 // document.write ("Percentage: " + percntg + "%");
- 
+
 
 //                           Question # 09
 
@@ -328,6 +328,8 @@
 
 
 
+// =======================Chapter # 06 To 09 ====================
+
 // =======================Chapter # 06 To 08 (Math expressions:Unfamiliar operators)====================
 
 
@@ -364,19 +366,20 @@
 
 
 
-// *************************Assignment_No_04_Chapter 9 to 11**************************
+
+// =======================Chapter # 06 To 09 ====================
 
 
 // =======================Chapter # 09 (Prompts)====================
 
 
-//                           Question # 01
+//                           Question # 03
 
 // var name = prompt("Enter Your Name");
 // alert ("Welcome to my website " + name);
 
 
-//                           Question # 02
+//                           Question # 04
 
 // var userNum = +prompt("Enter a Number that you want tabel", "5");
 
@@ -393,7 +396,7 @@
 // document.write (userNum + " x 10 = " + userNum*10);
 
 
-//                           Question # 03
+//                           Question # 05
 
 // var subject_1 = prompt("Enter the subject 1 name");
 // var subject_2 = prompt("Enter the subject 2 name");
@@ -447,3 +450,237 @@
 //                 + "</table>")
 
 
+
+
+// *************************Assignment_No_04_Chapter 9 to 11 and Chapter 12 to 13**************************
+
+// =======================Chapters # 9 To 11 ====================
+
+
+//                           Question # 01
+
+// var city = prompt ("Enter city name.");
+
+// if (city === "Karachi") {
+//  alert ("Welcome to city of lights");   
+// }
+
+//                           Question # 02
+
+// var gender = prompt("Enter your gender?");
+
+// if (gender === "male") {
+//     alert ("Good Morning Sir");
+// }
+
+// if (gender === "female") {
+//     alert ("Good Morning Ma'am");
+// }
+
+
+//                           Question # 03
+
+// var signalColor = prompt("Enter color of road traffic signal");
+
+// if (signalColor === "Red") {
+//     alert ("Must Stop");
+// }
+
+// if (signalColor === "Yellow") {
+//     alert ("Ready to move");
+// }
+
+// if (signalColor === "Green") {
+//     alert ("Move Now");
+// }
+
+
+//                           Question # 04
+
+// var remainingFule = prompt ("Enter remaining fule");
+
+// if (remainingFule < 0.25) {
+//     alert ("Please refill the fule in your car");
+// }
+
+
+//                           Question # 05
+
+// var a = 4; 
+// if (++a === 5) { 
+//     alert("given condition for variable a is true"); 
+// }
+
+// var b = 82;
+// if (b++ === 83) { 
+//     alert("given condition for variable b is true"); 
+// }
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+
+// if (++c < 14) {
+//     alert("condition 3 is true");
+// }
+
+// if (c === 14) { 
+//     alert("condition 4 is true"); 
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+
+// if (totalCost === laborCost + materialCost) {
+//     alert ("The cost equals");
+// }
+
+// if (true) {
+//     alert("True");
+// }
+
+// if (false) {
+//     alert("False");
+// }
+
+// if ("car" < "cat") {
+//     alert ("Car is smaller than cat");
+// }
+
+
+//                           Question # 06
+
+// var sub_1_ObtMarks = +prompt("Enter Subject 1 Obtained Marks");
+// var sub_1_TotalMarks = +prompt("Enter Subject 1 Total Marks");
+
+// var sub_2_ObtMarks = +prompt("Enter Subject 2 Obtained Marks");
+// var sub_2_TotalMarks = +prompt("Enter Subject 1 Total Marks");
+
+// var sub_3_ObtMarks = +prompt("Enter Subject 3 Obtained Marks");
+// var sub_3_TotalMarks = +prompt("Enter Subject 1 Total Marks");
+
+// var totalMarks = sub_1_TotalMarks + sub_2_TotalMarks + sub_3_TotalMarks;
+// var totalObtainedMarks = sub_1_ObtMarks + sub_2_ObtMarks + sub_3_ObtMarks;
+
+// var percentage = totalObtainedMarks / totalMarks * 100;
+
+// document.write ("<h1>Marks Sheet</h1>");
+// document.write ("Total marks: " + totalMarks + "<br>");
+// document.write ("Marks obtained: " + totalObtainedMarks + "<br>");
+// document.write ("Percentage: " + percentage + "%" + "<br>");
+
+// if (percentage >= 80) {
+//     document.write ("Grade: A-one" + "<br>");
+//     document.write ("Remarks: Excellent" + "<br>");
+// }
+
+// else if (percentage >= 70) {
+//     document.write ("Grade: A" + "<br>");
+//     document.write ("Remarks: Good" + "<br>");
+// }
+
+// else if (percentage >= 60) {
+//     document.write ("Grade: B" + "<br>");
+//     document.write ("Remarks: You need to improve" + "<br>");
+// }
+
+// else {
+//     document.write ("Grade: Fail" + "<br>");
+//     document.write ("Remarks: Sorry");
+// }
+
+
+//                           Question # 07
+
+// var num = 7;
+// var secrtNum = +prompt ("Enter one number from 1 to 10");
+// if (secrtNum === num) {
+//     alert ("Bingo! Correct answer");
+// }
+// else if (secrtNum === ++num) {
+//     alert ("Close enough to the correct answer");
+// }
+// else {
+//     alert("Try again");
+// }
+
+
+//                           Question # 08
+
+// var num = +prompt ("Enter the Number");
+
+// var divBy_3 = num % 3;
+
+// if (divBy_3 === 0) {
+//     alert (num + " is Divided by 3");
+// }
+// else {
+//     alert (num + " is not divided by 3");
+// }
+
+
+//                           Question # 09
+
+// var num = +prompt("Enter the Number");
+
+// var evenNum = num % 2;
+// if (evenNum === 0) {
+//     alert (num + " is even number");
+// }
+// else {
+//     alert (num + " is odd number");
+// }
+
+
+//                           Question # 10
+
+// var temp = +prompt("Enter temperature");
+
+// if (temp > 40) {
+//     alert("It is too hot outside");
+// }
+// else if (temp > 30) {
+//     alert ("The Weather today is Normal");
+// }
+// else if (temp > 20) {
+//     alert ("Today's Weather is Cool");
+// }
+// else if (temp > 10) {
+//     alert ("OMG! Today’s weather is so Cool");
+// }
+// else {
+//     alert ("OMG! Today’s weather is so Cool");
+// }
+
+
+//                           Question # 11
+
+// var firstNumber = +prompt ("Enter First Number");
+// var operator = prompt ("Enter operator \n +,-,*,/,%");
+// var secondNumber = +prompt ("Enter Second Number");
+
+// if (operator === "+") {
+//     alert (firstNumber + secondNumber);
+// }
+
+// else if (operator === "-") {
+//     alert (firstNumber - secondNumber);
+// }
+
+// else if (operator === "*") {
+//     alert (firstNumber * secondNumber);
+// }
+
+// else if (operator === "/") {
+//     alert (firstNumber / secondNumber);
+// }
+
+// else if (operator === "%") {
+//     alert (firstNumber % secondNumber);
+// }
